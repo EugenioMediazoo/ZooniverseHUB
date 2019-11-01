af2510c9f4dad9be13dea192fadf636e8849f8de
EugenioMediazoo <eugenio@mediazoo.tv>
2019-11-01 11:45:16 +0000

    [+] scene launcher [+] ration measure

--- HUB/Assets/Mediazoo/Scripts/UIManagerScript.cs
+++ HUB/Assets/Mediazoo/Scripts/UIManagerScript.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UI;
+using UnityEditor.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class UIManagerScript : MonoBehaviour
 {
@@ -26,24 +28,39 @@ public class UIManagerScript : MonoBehaviour
         screenHeight = Screen.height;
         CaseStudyStatus = false;
         AboutStatus = false;
+
+        float ratio = Screen.width / Screen.height;
+
+        if (ratio >= 0.45f)
+        {
+            Debug.Log("9:19.5");
+        }
+        else if (ratio >= 0.5f)
+        {
+            Debug.Log("9:18");
+        }
+        else if (ratio >= 0.55f)
+        {
+            Debug.Log("9:16");
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     public void CaseStudySlider()
     {
         if (!CaseStudyStatus)
         {
-            CaseStudy.transform.DOMoveY((screenHeight/2f), time).SetEase(Ease.OutBack);
+            CaseStudy.transform.DOMoveY((screenHeight / 2f), time).SetEase(Ease.OutBack);
             CaseStudyStatus = !CaseStudyStatus;
         }
-        else if(CaseStudyStatus)
+        else if (CaseStudyStatus)
         {
-            CaseStudy.transform.DOMoveY(((screenHeight/2f)*-1), time).SetEase(Ease.OutBack);
+            CaseStudy.transform.DOMoveY(((screenHeight / 2f) * -1), time).SetEase(Ease.OutBack);
             CaseStudyStatus = !CaseStudyStatus;
         }
     }
@@ -52,14 +69,28 @@ public class UIManagerScript : MonoBehaviour
     {
         if (!AboutStatus)
         {
-            About.transform.DOMoveY((screenHeight/2f), time).SetEase(Ease.OutBack);
+            About.transform.DOMoveY((screenHeight / 2f), time).SetEase(Ease.OutBack);
             AboutStatus = !AboutStatus;
         }
         else if (AboutStatus)
         {
-            About.transform.DOMoveY(((screenHeight/2f) * -1), time).SetEase(Ease.OutBack);
+            About.transform.DOMoveY(((screenHeight / 2f) * -1), time).SetEase(Ease.OutBack);
             AboutStatus = !AboutStatus;
         }
     }
 
+    public void ZooninverseScene()
+    {
+        GameObject _button = EventSystem.current.currentSelectedGameObject;
+        if (_button != null)
+        {
+            if (_button.CompareTag("Zooniverse"))
+            {
+                Debug.Log(_button.tag);
+            }
+        }
+        else
+            Debug.Log("currentSelectedGameObject is null");
     }
+
+}
