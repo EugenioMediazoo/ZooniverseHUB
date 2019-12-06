8a151218cd3a95a6b6e5ebd83a10deb6ed45cacf
EugenioMediazoo <eugenio@mediazoo.tv>
2019-12-06 17:47:07 +0000

    [+] Zooniverse AR foundation scene

--- HUB/Assets/Mediazoo/Scripts/Anim_Trigger.cs
+++ HUB/Assets/Mediazoo/Scripts/Anim_Trigger.cs
@@ -1,40 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Vuforia;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using Vuforia;
 
 
-public class Anim_Trigger : MonoBehaviour, ITrackableEventHandler
-{
+//public class Anim_Trigger : MonoBehaviour, ITrackableEventHandler
+//{
 
-    private TrackableBehaviour mTrackableBehaviour;
-    public GameObject animal_islands;
+//    private TrackableBehaviour mTrackableBehaviour;
+//    public GameObject animal_islands;
 
-    void Start()
-    {
-        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
-        if (mTrackableBehaviour)
-        {
-            mTrackableBehaviour.RegisterTrackableEventHandler(this);
-        }
-    }
+//    void Start()
+//    {
+//        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+//        if (mTrackableBehaviour)
+//        {
+//            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+//        }
+//    }
 
-    public void OnTrackableStateChanged(
-                                    TrackableBehaviour.Status previousStatus,
-                                    TrackableBehaviour.Status newStatus)
-    {
-        if (newStatus == TrackableBehaviour.Status.DETECTED ||
-            newStatus == TrackableBehaviour.Status.TRACKED ||
-            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
-        {
+//    public void OnTrackableStateChanged(
+//                                    TrackableBehaviour.Status previousStatus,
+//                                    TrackableBehaviour.Status newStatus)
+//    {
+//        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+//            newStatus == TrackableBehaviour.Status.TRACKED ||
+//            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+//        {
 
-            animal_islands.SetActive(true);
+//            animal_islands.SetActive(true);
 
-        }
-        else
-        {
-            animal_islands.SetActive(false);
+//        }
+//        else
+//        {
+//            animal_islands.SetActive(false);
 
-        }
-    }
-}
+//        }
+//    }
+//}
