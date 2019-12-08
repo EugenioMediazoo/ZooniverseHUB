3c9dd952c0693b4acc785ef1046da74afd2c6daf
emoggio <eugenio.moggio@gmail.com>
2019-12-08 19:29:55 +0000

    [+] ahrian changes

--- HUB/Assets/Mediazoo/Scripts/touchInput.cs
+++ HUB/Assets/Mediazoo/Scripts/touchInput.cs
@@ -1,71 +1,69 @@
-﻿//using UnityEngine;
-//using System.Collections;
-//using Vuforia;
+﻿using UnityEngine;
+using System.Collections;
 
 
-//public class touchInput : MonoBehaviour
+public class touchInput : MonoBehaviour
 
-//{
-//    public GameObject islands;
-//    //public GameObject london;
+{
+    //public GameObject london;
 
-//    public GameObject lioness;
-//    public GameObject gorilla;
-//    public GameObject canvas_Lioness;
-//    public GameObject canvas_Gorilla;
-
-
-//    public ParticleSystem lioness_Particles;
-//    public ParticleSystem gorilla_Particles;
+    public GameObject lioness;
+    public GameObject gorilla;
+    public GameObject canvas_Lioness;
+    public GameObject canvas_Gorilla;
 
 
-//    private bool lioness_tapped;
-//    private bool gorilla_tapped;
-
-//    private void Start()
-//    {
-//        lioness_tapped = false;
-//        gorilla_tapped = false;
-//        canvas_Lioness.SetActive(false);
-//        canvas_Gorilla.SetActive(false);
-
-//        //london.SetActive(false);
-//    }
-
-//    void Update()
-//    {
-//        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
-//        {
-//            Debug.Log("running");
+    public ParticleSystem lioness_Particles;
+    public ParticleSystem gorilla_Particles;
 
 
-//            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-//            RaycastHit raycastHit;
-//            if (Physics.Raycast(raycast, out raycastHit))
-//            {
+    private bool lioness_tapped;
+    private bool gorilla_tapped;
 
-//                if (raycastHit.collider.CompareTag("lioness"))
-//                {
-//                    Debug.Log("tapped");
-//                    lioness_Particles.Stop();
-//                    canvas_Lioness.SetActive(true);
-//                    lioness_tapped = true;
-//                }
+    private void Start()
+    {
+        lioness_tapped = false;
+        gorilla_tapped = false;
+        canvas_Lioness.SetActive(false);
+        canvas_Gorilla.SetActive(false);
 
-//                if (raycastHit.collider.CompareTag("gorilla"))
-//                {
-//                    Debug.Log("Gorilla tapped");
-//                    gorilla_Particles.Stop();
-//                    canvas_Gorilla.SetActive(true);
-//                    gorilla_tapped = true;
-//                }
-//            }
+        //london.SetActive(false);
+    }
 
-//            if (gorilla_tapped && lioness_tapped)
-//            {
-//                //london.SetActive(true);
+    void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Debug.Log("running");
 
-//            }
-//        }
-//    }
-//}
+
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+
+                if (raycastHit.collider.CompareTag("lioness"))
+                {
+                    Debug.Log("tapped");
+                    lioness_Particles.Stop();
+                    canvas_Lioness.SetActive(true);
+                    lioness_tapped = true;
+                }
+
+                if (raycastHit.collider.CompareTag("gorilla"))
+                {
+                    Debug.Log("Gorilla tapped");
+                    gorilla_Particles.Stop();
+                    canvas_Gorilla.SetActive(true);
+                    gorilla_tapped = true;
+                }
+            }
+
+            if (gorilla_tapped && lioness_tapped)
+            {
+                //london.SetActive(true);
+
+            }
+        }
+    }
+}
