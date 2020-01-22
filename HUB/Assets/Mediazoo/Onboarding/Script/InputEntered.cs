8a61b3721ba033d363b720cb797ac4ba7135069e
EugenioMediazoo <eugenio@mediazoo.tv>
2020-01-22 18:12:44 +0000

    [+] input entered realign text

---
+++ HUB/Assets/Mediazoo/Onboarding/Script/InputEntered.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InputEntered : MonoBehaviour
+{
+    public GameObject textContainer;
+    public GameObject textBG;
+    private LayoutGroup ContainerAlgn;
+
+    public Sprite NewBG;
+    private Image CurrentBG;
+
+    public TextMeshProUGUI TextColor;
+
+    private void Awake()
+    {
+        ContainerAlgn = textContainer.GetComponent<VerticalLayoutGroup>();
+        CurrentBG = textBG.GetComponent<Image>();
+        //Debug.Log(ContainerAlgn);
+    }
+
+    public void ShiftBubble()
+    {
+        ContainerAlgn.childAlignment = TextAnchor.MiddleRight;
+        TextColor.color = Color.white;
+
+        CurrentBG.sprite = NewBG;
+        //ContainerAlgn = MiddleRight;
+    }
+}
