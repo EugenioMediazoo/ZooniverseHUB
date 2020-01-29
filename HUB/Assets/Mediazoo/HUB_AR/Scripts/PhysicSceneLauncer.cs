e1932bc6ed0bf9b151b1f83c33f893f9cdeaad72
EugenioMediazoo <eugenio@mediazoo.tv>
2020-01-29 17:22:43 +0000

    [+] spatial HUB

---
+++ HUB/Assets/Mediazoo/HUB_AR/Scripts/PhysicSceneLauncer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PhysicSceneLauncer: MonoBehaviour
+{
+    public string ZooniverseAR;
+    public string Onboarding;
+    public string Hub;
+    public string AROnboarding;
+    public string ARGallery;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Zooniverse"))
+        {
+            SceneManager.LoadScene(ZooniverseAR, LoadSceneMode.Single);
+            Debug.Log(other.tag);
+        }
+        else if (other.CompareTag("Hub"))
+        {
+            SceneManager.LoadScene(Hub, LoadSceneMode.Single);
+            Debug.Log(other.tag);
+        }
+        else if (other.CompareTag("Onboarding"))
+        {
+            SceneManager.LoadScene(Onboarding, LoadSceneMode.Single);
+            Debug.Log(other.tag);
+        }
+        else if (other.CompareTag("ARGallery"))
+        {
+            SceneManager.LoadScene(ARGallery, LoadSceneMode.Single);
+            Debug.Log(other.tag);
+        }
+        else if (other.CompareTag("AROnboarding"))
+        {
+            SceneManager.LoadScene(AROnboarding, LoadSceneMode.Single);
+            Debug.Log(other.tag);
+        }
+    }
+}
